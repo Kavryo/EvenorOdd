5f7aa4d2c85780749fb98bdfdedf0bdad286034a
Kavya Bhatt <kavyabhatt28aug@gmail.com>
2022-04-21 21:38:53 +0530

    Java program to tell if a user given no. is even or odd.

---
+++ src/com/OddEven/Main.java
@@ -0,0 +1,19 @@
+package com.OddEven;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Gimme a number: ");
+        Scanner userInput = new Scanner(System.in);
+        int n = userInput.nextInt();
+        if(n % 2 == 0)
+        {
+            System.out.println(n + " is Even.");
+        }
+        else
+        {
+            System.out.println(n + " is Odd.");
+        }
+    }
+}
